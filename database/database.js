886ed3fed6f4f61f9ac25ad5c5aae337dedfac35
andrewbartkomacewan <bartkoa@mymacewan.ca>
2024-02-14 14:21:37 -0700

    Second Commit changing Database connection to MongoDb Atlas

--- database/database.js
+++ database/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 mongoose.set('strictQuery', true);
 
 export const connectDB = async () => {
-    const url = `mongodb://localhost:27017/cmpt315`;
+    const url = `mongodb+srv://bartkoa:EnBhXkHYu9QeaQi4@cluster0.lrqgsxo.mongodb.net/?retryWrites=true&w=majority`;
     try {
         const connection = await mongoose.connect(url);
         console.log("Database connected successfully");
