502ad92efc9e24ab150f4b44e715147a4adc7510
andrewbartkomacewan <bartkoa@mymacewan.ca>
2024-02-14 20:56:57 -0700

    Third commit fix for render

--- database/database.js
+++ database/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 mongoose.set('strictQuery', true);
 
 export const connectDB = async () => {
-    const url = `mongodb+srv://bartkoa:EnBhXkHYu9QeaQi4@cluster0.lrqgsxo.mongodb.net/?retryWrites=true&w=majority`;
+    const url = "mongodb+srv://bartkoa:EnBhXkHYu9QeaQi4@cluster0.lrqgsxo.mongodb.net/";
     try {
         const connection = await mongoose.connect(url);
         console.log("Database connected successfully");
